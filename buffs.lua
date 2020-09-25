677509f53b013a70225ff3fa0fe9bcff82a4605e
prowley <parowley@gmail.com>
2020-09-24 18:11:36 -0700

    Create buffs.lua
    Worlds buffs in array form. For the future.

---
+++ buffs.lua
@@ -0,0 +1,54 @@
+City = {
+  "WarChiefs Blessing",
+  "Rallying Cry of the Dragonslayer",
+  "Spirit of Zandalar",
+}
+
+DarkMoon = {
+  "Sayge's Dark Fortune of Damage",
+  "Sayge's Dark Fortune of Resistance",
+  "Sayge's Dark Fortune of Armor",
+  "Sayge's Dark Fortune of Intelligence",
+  "Sayge's Dark Fortune of Spirit",
+  "Sayge's Dark Fortune of Stamina",
+  "Sayge's Dark Fortune of Strength",
+  "Sayge's Dark Fortune of Agility",
+}
+
+DireMaul = {
+  "Mol'dar's Moxie",
+  "Fengus' Ferocity",
+  "Slip'kik's Savvy",
+}
+
+Felwood = {
+  "Songflower Serenade",
+  "Windblossom Berries",
+  "Whipper Root Tuber",
+  "Night Dragon's Breath",
+}
+
+BlastedLands = {
+  "R.O.I.D.S.",
+  "Ground Scorpak Assay",
+  "Lung Juice Cocktail",
+  "Cerebral Cortex Compound",
+  "Gizzard Gum",
+}
+
+Winterspring = {
+  "Juju Might",
+  "Juju Power",
+  "Juju Flurry",
+  "Juju Gulie",
+  "Juju Escape",
+  "Juju Chill",
+  "Juju Ember"
+}
+
+TimeSensitive = {
+  "City",
+  "DarkMoon",
+  "DireMaul",
+  "Felwood"
+}
